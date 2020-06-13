c19b257f9639c8e62e4405fafc2bce0685a2be47
Ebuka Umeokonkwo <ebukaume@gmail.com>
2020-06-13 09:05:46 +0100

    refactor[database]: switch from test database

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,7 @@ const Author = require('./author');
 const Book = require('./book');
 const Subject = require('./subject');
 
-mongoose.connect('mongodb://localhost:27017/test',
+mongoose.connect('mongodb://localhost:27017/books',
   { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = { Author, Book, Subject };
